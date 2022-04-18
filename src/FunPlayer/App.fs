18848530ce7154a71b31129f7deded435adee2d2
Angel D. Munoz <scythes.lol@gmail.com>
2022-04-17 21:02:48 -0500

    improve media player

--- src/FunPlayer/App.fs
+++ src/FunPlayer/App.fs
@@ -28,10 +28,31 @@ type PlayState =
 
 [<Struct>]
 type LoopState =
-  | Stop
+  | Off
   | One
   | All
   | Random
+
+  member this.AsString =
+    match this with
+    | Off -> "Stop"
+    | One -> "One"
+    | All -> "All"
+    | Random -> "Random"
+
+  static member FromString value =
+    match value with
+    | "off"
+    | "Off" -> ValueSome Off
+    | "one"
+    | "One" -> ValueSome One
+    | "all"
+    | "All" -> ValueSome All
+    | "random"
+    | "Random" -> ValueSome Random
+    | value ->
+      printfn $"Invalid Loop State: {value}"
+      ValueNone
 
 [<Struct>]
 type PlayerState =
@@ -49,7 +70,7 @@ module PlayerState =
       duration = 0.
       currentTime = 0.
       playState = Stopped
-      loopState = Stop }
+      loopState = Off }
 
   let (|IsReady|IsPlaying|IsPaused|ItStoped|) str =
     match str with
@@ -61,12 +82,37 @@ module PlayerState =
     | "IsReady" -> IsReady
     | "it-stoped"
     | "ItStoped" -> ItStoped
-    | _ -> failwith "olv"
+    | status -> failwith $"Invalid Status: {status}"
+
+  let (|Off|All|One|Random|) value =
+    match value with
+    | "off"
+    | "Off" -> Off
+    | "one"
+    | "One" -> One
+    | "all"
+    | "All" -> All
+    | "random"
+    | "Random" -> Random
+    | value -> failwith $"Invalid Loop State: {value}"
 
   let state = cval ValueNone
   let playlist = cval Array.empty<Song>
   let setSong (song: PlayerState voption) = state.Publish song
+
   let setSongs (songs: Song []) = playlist.Publish songs
+
+  let updateLoopState loopState =
+    let loopState = LoopState.FromString loopState
+
+    state.Publish (fun state ->
+      ValueOption.map
+        (fun state ->
+          { state with
+              loopState =
+                loopState
+                |> ValueOption.defaultValue state.loopState })
+        state)
 
   [<JSInvokable>]
   let updatePlayStatus status =
@@ -101,7 +147,7 @@ module PlayerState =
 
 module Components =
   module Player =
-    let playlist (songs: Song [] cval) (onplaySong: Song -> unit) =
+    let playlist (songs: cval<Song []>) (onplaySong: Song -> unit) =
       html.inject (
         "fun-playlist",
         fun () ->
@@ -111,20 +157,22 @@ module Components =
             aside {
               class' "fun-playlist"
 
-              virtualize (
-                songs,
-                fun song ->
+              Virtualize'() {
+                Items songs
+
+                childContent (fun song ->
                   li {
+                    class' "fun-playlist-item"
                     ondblclick (fun _ -> onplaySong song)
                     song.name
-                  }
-              )
+                  })
+              }
             }
           }
       )
 
     let mediaBar
-      (state: PlayerState cval)
+      (state: PlayerState aval)
       (onPlay: unit -> Task)
       (onPause: unit -> Task)
       (onUnpause: unit -> Task)
@@ -133,76 +181,91 @@ module Components =
       (onLoopChange: LoopState -> unit)
       =
 
+      let mediaButtons (state: PlayState) =
+        section {
+          class' "fun-mb-state-btns"
+
+          match state with
+          | Paused ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onBack ())
+              "Back"
+            }
+
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onUnpause ())
+              "Play"
+            }
+          | Playing ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onBack ())
+              "Back"
+            }
+
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onPause ())
+              "Pause"
+            }
+
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onNext ())
+              "onNext"
+            }
+          | Ready
+          | Stopped ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onPlay ())
+              "Play"
+            }
+        }
+
+      let loopButtons (state: LoopState) =
+        section {
+          class' "fun-mb-loop-btns"
+
+          match state with
+          | Off ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onLoopChange One)
+              "Off"
+            }
+          | One ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onLoopChange All)
+              "One"
+            }
+          | All ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onLoopChange Random)
+              "All"
+            }
+          | Random ->
+            button {
+              class' "fun-media-btn"
+              onclick (fun _ -> onLoopChange Off)
+              "Random"
+            }
+        }
+
+
       html.inject (
         "fun-mb",
         fun () ->
           adaptiview () {
-            let! state = state
-
-            let mediaButtons =
-              section {
-                class' "fun-mb-state-btns"
-
-                match state.playState with
-                | Paused ->
-                  button {
-                    onclick (fun _ -> onBack ())
-                    "Back"
-                  }
-
-                  button {
-                    onclick (fun _ -> onUnpause ())
-                    "Play"
-                  }
-                | Playing ->
-                  button {
-                    onclick (fun _ -> onBack ())
-                    "Back"
-                  }
-
-                  button {
-                    onclick (fun _ -> onPause ())
-                    "Pause"
-                  }
-
-                  button {
-                    onclick (fun _ -> onNext ())
-                    "onNext"
-                  }
-                | Ready
-                | Stopped ->
-                  button {
-                    onclick (fun _ -> onPlay ())
-                    "Play"
-                  }
-              }
-
-            let loopButtons =
-              section {
-                class' "fun-mb-loop-btns"
-
-                match state.loopState with
-                | Stop ->
-                  button {
-                    onclick (fun _ -> onLoopChange One)
-                    "Off"
-                  }
-                | One ->
-                  button {
-                    onclick (fun _ -> onLoopChange All)
-                    "One"
-                  }
-                | All ->
-                  button {
-                    onclick (fun _ -> onLoopChange Random)
-                    "All"
-                  }
-                | Random ->
-                  button {
-                    onclick (fun _ -> onLoopChange Stop)
-                    "Random"
-                  }
-              }
+            let! playState = AVal.map (fun value -> value.playState) state
+            let! loopState = AVal.map (fun value -> value.loopState) state
+            let! currentTime = AVal.map (fun value -> value.currentTime) state
+            let! duration = AVal.map (fun value -> value.duration) state
+            let! songName = AVal.map (fun value -> value.name) state
 
             nav {
               class' "fun-mb"
@@ -210,22 +273,30 @@ module Components =
               section {
                 class' "fun-mb-media-group"
 
+                h4 {
+                  class' "song-title"
+                  $"{songName}"
+                }
+
                 input {
-                  class' "fun-mb-slider"
+                  class' "song-slider"
                   type' "range"
-                  value state.currentTime
+                  value currentTime
                   step "any"
                   min 0
-                  max state.duration
+                  max duration
 
                   onchange (fun event ->
                     printfn $"Seek %f{event.Value |> string |> float}")
                 }
-
-                mediaButtons
               }
 
-              loopButtons
+              section {
+                class' "fun-mb-media-buttons"
+                mediaButtons playState
+                loopButtons loopState
+              }
+
             }
           }
       )
@@ -249,10 +320,10 @@ module Components =
           menu {
             class' (titlebarClass hasOverlay)
 
-            li {
+            button {
               class' "fun-title-bar-menu-item"
               onclick (fun _ -> onOpenDirectory ())
-              b { "Open Directory" }
+              "Select Music Directory"
             }
           }
         }
@@ -272,7 +343,6 @@ module App =
 
         hook.OnFirstAfterRender
         |> Observable.map (fun _ ->
-          printfn "Olv"
           browser.supportsWindowControlsOverlay().AsTask())
         |> Observable.switchTask
         |> Observable.subscribe _withOverlay.Publish
@@ -280,18 +350,18 @@ module App =
 
         let onNext () = task { return () } :> Task
         let onBack () = task { return () } :> Task
-        let onLoopChange loopState = ()
+
+        let onLoopChange (loopState: LoopState) =
+          PlayerState.updateLoopState loopState.AsString
 
         adaptiview () {
           let! playlist = PlayerState.playlist
           let _withOverlay = hook.UseCVal _withOverlay
           let! withOverlay = _withOverlay
-          let! playerState = PlayerState.state
 
           let playerState =
-            playerState
-            |> ValueOption.defaultValue PlayerState.empty
-            |> cval
+            PlayerState.state
+            |> AVal.map (fun value -> defaultValueArg value PlayerState.empty)
 
           let loadSongs () =
             task {
--- src/FunPlayer/Extensions.fs
+++ src/FunPlayer/Extensions.fs
@@ -2,18 +2,69 @@
 module FunPlayer.Extensions
 
 open Fun.Blazor
-open Microsoft.AspNetCore.Components
+open Fun.Blazor.Operators
 open Microsoft.AspNetCore.Components.Web.Virtualization
-open System.Collections.Generic
 
+type VirtualizeBuilder<'FunBlazorGeneric, 'TItem when 'FunBlazorGeneric :> Microsoft.AspNetCore.Components.IComponent>
+  () =
+  inherit ComponentWithDomAndChildAttrBuilder<'FunBlazorGeneric>()
 
-let inline virtualize<'T>
-  (
-    items: ICollection<'T>,
-    [<InlineIfLambda>] template: 'T -> NodeRenderFragment
-  ) =
-  html.blazor (fun ctx ->
-    Virtualize(
-      Items = items,
-      ChildContent = RenderFragment<'T>(fun item -> ctx.Render(template item))
-    ))
+  [<CustomOperation("childContent")>]
+  member inline _.childContent
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      fn: 'TItem -> NodeRenderFragment
+    ) =
+    render
+    ==> html.renderFragment ("ChildContent", fn)
+
+  [<CustomOperation("ItemContent")>]
+  member inline _.ItemContent
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      fn: 'TItem -> NodeRenderFragment
+    ) =
+    render ==> html.renderFragment ("ItemContent", fn)
+
+  [<CustomOperation("Placeholder")>]
+  member inline _.Placeholder
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      fn: PlaceholderContext -> NodeRenderFragment
+    ) =
+    render ==> html.renderFragment ("Placeholder", fn)
+
+  [<CustomOperation("ItemSize")>]
+  member inline _.ItemSize
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      x: System.Single
+    ) =
+    render ==> ("ItemSize" => x)
+
+  [<CustomOperation("ItemsProvider")>]
+  member inline _.ItemsProvider
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      x: ItemsProviderDelegate<'TItem>
+    ) =
+    render ==> ("ItemsProvider" => x)
+
+  [<CustomOperation("Items")>]
+  member inline _.Items
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      x: System.Collections.Generic.ICollection<'TItem>
+    ) =
+    render ==> ("Items" => x)
+
+  [<CustomOperation("OverscanCount")>]
+  member inline _.OverscanCount
+    (
+      [<InlineIfLambda>] render: AttrRenderFragment,
+      x: System.Int32
+    ) =
+    render ==> ("OverscanCount" => x)
+
+type Virtualize'<'TItem>() =
+  inherit VirtualizeBuilder<Virtualize<'TItem>, 'TItem>()
